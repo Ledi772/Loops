1cc62043c70d1a7ece4e916d124430abee47faf6
Ledi772 <halilijuled2@gmail.com>
2024-12-02 09:45:41 -0800

    'tesst'

---
+++ javascript.js
@@ -0,0 +1,18 @@
+// // console.log 
+// for (let i = 9; i > 4; i--) {
+//     console.log(i);
+// }
+
+// for (let i = 9; i > 4; i += 2) {
+//     console.log(i);
+// }
+
+for (let i = 0; i < 101; i += 2) {
+    console.log(i);
+}
+
+
+
+// git add .
+// git commit -m 'kryta'
+// git push
