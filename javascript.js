9b74791bd452f5263102063a1b21ce163c0549b5
Ledi772 <halilijuled2@gmail.com>
2024-12-03 09:43:25 -0800

    'dfsdfsdfsdf'

--- javascript.js
+++ javascript.js
@@ -7,12 +7,33 @@
 //     console.log(i);
 // }
 
-for (let i = 0; i < 101; i += 2) {
-    console.log(i);
-}
+// for (let i = 0 ; i < 101; i += 2) {
+//     console.log(i);
+// }
 
+
+// if (numir >= 0){
+//     if duhet me edhe else tani else if ose esle if tjeter
+// }
 
 
 // git add .
 // git commit -m 'kryta'
 // git push
+
+
+// for (let i = 1; i <= 8 ; i++) {
+//     if (i === 7) {
+//         continue; 
+//     }
+//     console.log(i);
+// }
+
+let start, _stop
+start = prompt ("prej cilit numer po don mja nise me printu ne konsoller")
+_stop = prompt ("deri te cili numer po don mu printu")
+
+for (let i = start; i > _stop; i++) {
+    console.log(i)
+    
+}
